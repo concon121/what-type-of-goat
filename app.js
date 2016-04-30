36b449a4b40cc455307cd1fb7a15e5c988413300
Connor Bray <connor.bray@icloud.com>
2016-04-30 08:30:10 +0100

    Add more goats! Random goats.

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+function randomResult() {
+  var goats =  [{"text": "You are a Maris Piper Potato.", "img": "potato.jpeg"},
+                {"text": "You are a baby goat!", "img": "baby-goat.jpg"},
+                {"text": "You are a beardy goat!", "img": "beardy-goat.jpg"},
+                {"text": "You are a horny goat!", "img": "horny-goat.jpg"},
+                {"text": "You are an extreme goat!", "img": "extreme-goat.jpg"}];
+  return goats[Math.round(Math.random() * (goats.length - 1))];
+}
+
 function create(index) {
   return function(e) {
     e.preventDefault();
@@ -6,6 +15,9 @@ function create(index) {
         $("#question" + (index+1)).show("slide", null, 1000, null);
       } else {
         $("#title").empty().append("<h1>Results</h1>");
+        var goat = randomResult();
+        $("#resultText").empty().append(goat.text);
+        $("#resultImg").attr("src", goat.img);
         $("#results").show("slide", null, 1000, null);
       }
     });
